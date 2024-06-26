d18783e63bba7180089cc910db8ca7ebd274bde0
LEGENDcmbk <muhammadayubmahmudov393@gmail.com>
2024-06-26 08:03:32 +0500

    Next config updated

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,21 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+    // next.config.js
+    module.exports = {
+        webpack: (config, { dev }) => {
+        if (dev) {
+            config.module.rules.forEach((rule) => {
+            if (rule.test && rule.test.toString().includes('.css')) {
+                rule.use.forEach((use) => {
+                if (use.loader && use.loader.includes('css-loader')) {
+                    delete use.options.sourceMap;
+                }
+                });
+            }
+            });
+        }
+            return config;
+        },
+    };

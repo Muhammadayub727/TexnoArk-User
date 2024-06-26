8dafdbc71737d02a27be8796a1cca5121b254c1c
LEGENDcmbk <muhammadayubmahmudov393@gmail.com>
2024-06-26 05:39:48 +0500

    Project Updated

--- app/page.tsx
+++ app/page.tsx
@@ -5,7 +5,7 @@ import Container from '../components/container/page'
 import './globals.css'
 import {Button } from 'antd';
 import Image from 'next/image';
-import Footer_Banner from '../images/Footer_Banner.png'
+import Footer_Banner from '../assets/Footer_Banner.png'
 import Swiper from '../components/swiper/page'
 import useCategoryStore from '@/store/categories/page';
 import { useEffect } from 'react';
@@ -24,59 +24,59 @@ export default function Home() {
       </div>
 
       <div className='mt-[60px] '>
-         <Container>
-           <div className='flex items-center flex-wrap justify-between gap-[20px]'>
-                {
-                  arr.map((e, i) => {
-                    return (
-                      <div key={i} className='flex items-center justify-between w-[440px] h-[120px] py-[45px] px-[59px] bg-white rounded-xl cursor-pointer card'>
-                          <PhoneOutlined className='w-[60px] h-[60px] bg-[#FF800B1A] rounded-full p-[20px] text-[#D55200]'/>
-                          Smartfonlar
-                          <ArrowRightOutlined />
-                      </div>
-                    )
-                  })   
-                }
-           </div>
-         </Container>
+          <Container>
+            <div className='flex items-center flex-wrap justify-between gap-[20px]'>
+                  {
+                    arr.map((e, i) => {
+                      return (
+                        <div key={i} className='flex items-center justify-between w-[440px] h-[120px] py-[45px] px-[59px] bg-white rounded-xl cursor-pointer card'>
+                            <PhoneOutlined className='w-[60px] h-[60px] bg-[#FF800B1A] rounded-full p-[20px] text-[#D55200]'/>
+                            Smartfonlar
+                            <ArrowRightOutlined />
+                        </div>
+                      )
+                    })   
+                  }
+            </div>
+          </Container>
+        </div>
+
+        <div className='mt-[64px]'>
+          <Container>
+            <div className='flex justify-between items-center'>
+              <h1 className='font-bold text-[32px] '>Popular Product</h1>
+              <Button className='btn_all'>Hammasi <ArrowRightOutlined /></Button>
+            </div>
+
+            <div className='mt-[24px]'>
+                <Swiper/>
+            </div>
+          </Container>
       </div>
 
       <div className='mt-[64px]'>
-        <Container>
-           <div className='flex justify-between items-center'>
-             <h1 className='font-bold text-[32px] '>Ommabob mahsulotlar</h1>
-             <Button className='btn_all'>Hammasi <ArrowRightOutlined /></Button>
-           </div>
+          <Container>
+            <div className='flex justify-between items-center'>
+              <h1 className='font-bold text-[32px] '>Bestsellers</h1>
+              <Button className='btn_all'>Hammasi <ArrowRightOutlined /></Button>
+            </div>
 
-           <div className='mt-[24px]'>
-              <Swiper/>
-           </div>
-        </Container>
-      </div>
+            <div className='mt-[24px]'>
+            <Swiper/>
+            </div>
+          </Container>
+        </div>  
 
-      <div className='mt-[64px]'>
-        <Container>
-           <div className='flex justify-between items-center'>
-             <h1 className='font-bold text-[32px] '>Populyarniy mahsulotlar</h1>
-             <Button className='btn_all'>Hammasi <ArrowRightOutlined /></Button>
-           </div>
+        <div className='mt-[64px]'>
+          <Container>
+            <div className='flex justify-between items-center'>
+              <h1 className='font-bold text-[32px] '>Promotional product</h1>
+              <Button className='btn_all'>Hammasi <ArrowRightOutlined /></Button>
+            </div>
 
-           <div className='mt-[24px]'>
-           <Swiper/>
-           </div>
-        </Container>
-      </div>  
-
-      <div className='mt-[64px]'>
-        <Container>
-           <div className='flex justify-between items-center'>
-             <h1 className='font-bold text-[32px] '>Aksiyadagi mahsulotlar</h1>
-             <Button className='btn_all'>Hammasi <ArrowRightOutlined /></Button>
-           </div>
-
-           <div className='mt-[24px]'>
-           <Swiper/>
-           </div>
+            <div className='mt-[24px]'>
+            <Swiper/>
+            </div>
         </Container>
       </div>  
 
--- components/header/page.tsx
+++ components/header/page.tsx
@@ -1,30 +1,188 @@
+// "use client"
+// import React, { useEffect } from "react";
+// import Container from "../container/page";
+// import Image from "next/image";
+// import LOGO from "../../assets/LOGO.png";
+// import { Button, Input, Avatar, Badge } from "antd";
+// import { ArrowRightOutlined, BarChartOutlined, CloseSquareOutlined, HeartOutlined, PhoneOutlined, SearchOutlined, ShoppingCartOutlined, UnorderedListOutlined, UserOutlined } from "@ant-design/icons";
+// import "./style.css";
+// import useCategoryStore from '@/store/categories/page';
+// import useSubCategoryStore from "@/store/sub-categories/page";
+
+// import { useState } from "react";
+
+
+
+// function Index() {
+//   const [open, setOpen] = useState(false)
+//   const {categories, getCategories} = useCategoryStore()
+//   const {subcategories, getSubCategories} = useSubCategoryStore()
+
+//   async function getSub(id: number){
+//     await getSubCategories(id)
+//   }
+
+//   useEffect(() =>{
+//     getCategories()
+//   }, [])
+//   return (
+//     <header>
+//       <div className="py-[10px] bg-[#F0F0F0]">
+//         <Container>
+//           <div className="flex justify-between items-center">
+//             <ul className="flex items-center gap-[20px]">
+//               <li className="text-[14px] font-medium cursor-pointer">
+//                 Biz Haqimizda
+//               </li>
+//               <li className="text-[14px] font-medium cursor-pointer">
+//                 Yetkazib berish
+//               </li>
+//               <li className="text-[14px] font-medium cursor-pointer">
+//                 Shartnoma shartlari
+//               </li>
+//               <li className="text-[14px] font-medium cursor-pointer">
+//                 Bizning kafolatlar
+//               </li>
+//             </ul>
+//             <ul className="flex items-center gap-[15px]">
+//               <li className="text-[14px] font-medium ">+998 91 043 45 49</li>
+//               <li className="px-[10px] py-[4px] bg-[white] rounded-lg cursor-pointer font-medium text-[#240E0066]">
+//                 Rus
+//               </li>
+//               <li className="px-[10px] py-[4px] bg-[white] rounded-lg cursor-pointer font-medium text-[#240E0066]">
+//                 O'zb
+//               </li>
+//               <li className="px-[10px] py-[4px] bg-[white] rounded-lg cursor-pointer font-medium text-[#240E0066]">
+//                 Eng
+//               </li>
+//             </ul>
+//           </div>
+//         </Container>
+//       </div>
+      
+      
+//       <div>
+//         <Container>
+//           <div className="py-[30px] px-[60px] flex justify-between bg-[white] rounded-md relative">
+//             <Image
+//               className="ml-[30px]"
+//               src={LOGO}
+//               width={140}
+//               height={40}
+//               alt="Picture of the author"
+//             />
+
+//             <div className="flex gap-[16px] items-center">
+//               <Button onClick={() => setOpen(!open)} className="category_btn bg-[#1EB91E] w-[180px] text-white text-[14px] font-bold py-[15px] px-[36px] h-[46px]">
+//                   {
+//                     open?
+//                     <CloseSquareOutlined className=" text-[24px]"/>
+//                     :
+//                     <UnorderedListOutlined className=" text-[18px] rotate-180" />
+//                   }
+//                   {
+//                     open?
+//                     "Закрывать"
+//                     :
+//                     "Категория"
+//                   }
+//               </Button>
+//               <Input
+//                 placeholder="Kупить xочу.. "
+//                 className="search_input text-[14px] font-medium w-[520px] h-[46px] py-[15px] px-[36px] rounded-md"
+//                 prefix={<SearchOutlined />}
+//                 // suffix={<CloseSquareOutlined />}
+//               />
+//             </div>
+
+//             <div className={`w-[100%] flex gap-[80px] absolute bg-[#fff] left-0 z-20 py-[30px] px-[20px] ${open ? ' top-[110px] duration-300 opacity-1' : "top-[-1200px] duration-300 opacity-0"} drawer`}>
+//                 <div>
+//                     {
+//                       categories.map((e,i) => {
+//                         return (
+//                           <div key={i} onClick={() => getSub(e.id)} className=' hover:bg-[#FF6F14] hover:text-white mt-[10px] flex items-center justify-between w-[440px] h-[50px] py-[35px] px-[59px] bg-[#F0F0F0] rounded-xl cursor-pointer card '>
+//                               <PhoneOutlined className='w-[60px] h-[60px] text-[20px]'/>
+//                               <p>{e.name}</p>
+//                               <ArrowRightOutlined />
+//                           </div>
+//                         )
+//                       })
+//                     }
+//                 </div>
+//                 <div className="mt-[7px] border-l-[1px] pl-[60px]">
+//                     <p className="text-[27px] font-bold mb-[20px]">Tovarlar</p>
+//                     {
+//                       subcategories.map((e,i) => {
+//                         return (
+//                           <div key={i}>
+//                                 <p className="font-semibold text-[16px] mb-4 cursor-pointer">{e.name}</p>
+//                           </div>
+//                         )
+//                       })
+//                     }
+//                 </div>
+//             </div>
+
+//             <div className="flex items-center gap-[15px]">
+//               <Badge count={2} >
+//                 <Avatar shape="square" size="large" className="bg-[#F0F0F0] cursor-pointer" >
+//                     <HeartOutlined className="text-[20px] text-[black]" />
+//                 </Avatar>
+//               </Badge>
+//               <Badge count={6} >
+//                 <Avatar shape="square" size="large" className="bg-[#F0F0F0] cursor-pointer">
+//                     <BarChartOutlined className="text-[20px] text-[black]"/>
+//                 </Avatar>
+//               </Badge>
+//               <Badge count={7} >
+//                 <Avatar shape="square" size="large" className="bg-[#F0F0F0] cursor-pointer">
+//                     <ShoppingCartOutlined className="text-[20px] text-[black]" />
+//                 </Avatar>
+//               </Badge>
+//               <Avatar size="large" icon={<UserOutlined className="text-[20px] text-[black]"/>} className="bg-[#F0F0F0] cursor-pointer"/>
+//             </div>
+//           </div>
+//         </Container>
+//       </div>
+//     </header>
+//   );
+// }
+
+// export default Index;
 "use client"
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import Container from "../container/page";
 import Image from "next/image";
 import LOGO from "../../assets/LOGO.png";
 import { Button, Input, Avatar, Badge } from "antd";
-import { ArrowRightOutlined, BarChartOutlined, CloseSquareOutlined, HeartOutlined, PhoneOutlined, SearchOutlined, ShoppingCartOutlined, UnorderedListOutlined, UserOutlined } from "@ant-design/icons";
+import { 
+  ArrowRightOutlined, 
+  BarChartOutlined, 
+  CloseSquareOutlined, 
+  HeartOutlined, 
+  PhoneOutlined, 
+  SearchOutlined, 
+  ShoppingCartOutlined, 
+  UnorderedListOutlined, 
+  UserOutlined 
+} from "@ant-design/icons";
 import "./style.css";
 import useCategoryStore from '@/store/categories/page';
 import useSubCategoryStore from "@/store/sub-categories/page";
 
-import { useState } from "react";
-
-
-
 function Index() {
-  const [open, setOpen] = useState(false)
-  const {categories, getCategories} = useCategoryStore()
-  const {subcategories, getSubCategories} = useSubCategoryStore()
+  const [open, setOpen] = useState(false);
+  const { categories, getCategories } = useCategoryStore();
+  const { subcategories, getSubCategories } = useSubCategoryStore();
 
-  async function getSub(id: number){
-    await getSubCategories(id)
+  async function getSub(id: number) {
+    await getSubCategories(id);
   }
 
-  useEffect(() =>{
-    getCategories()
-  }, [])
+  useEffect(() => {
+    getCategories();
+  }, []);
+
   return (
     <header>
       <div className="py-[10px] bg-[#F0F0F0]">
@@ -45,7 +203,7 @@ function Index() {
               </li>
             </ul>
             <ul className="flex items-center gap-[15px]">
-              <li className="text-[14px] font-medium ">+998 91 043 45 49</li>
+              <li className="text-[14px] font-medium">+998 91 043 45 49</li>
               <li className="px-[10px] py-[4px] bg-[white] rounded-lg cursor-pointer font-medium text-[#240E0066]">
                 Rus
               </li>
@@ -59,8 +217,7 @@ function Index() {
           </div>
         </Container>
       </div>
-      
-      
+
       <div>
         <Container>
           <div className="py-[30px] px-[60px] flex justify-between bg-[white] rounded-md relative">
@@ -73,73 +230,65 @@ function Index() {
             />
 
             <div className="flex gap-[16px] items-center">
-              <Button onClick={() => setOpen(!open)} className="category_btn bg-[#1EB91E] w-[180px] text-white text-[14px] font-bold py-[15px] px-[36px] h-[46px]">
-                  {
-                    open?
-                    <CloseSquareOutlined className=" text-[24px]"/>
-                    :
-                    <UnorderedListOutlined className=" text-[18px] rotate-180" />
-                  }
-                  {
-                    open?
-                    "Закрывать"
-                    :
-                    "Категория"
-                  }
+              <Button 
+                onClick={() => setOpen(!open)} 
+                className="category_btn bg-[#1EB91E] w-[180px] text-white text-[14px] font-bold py-[15px] px-[36px] h-[46px]"
+              >
+                {open ? (
+                  <CloseSquareOutlined className="text-[24px]" />
+                ) : (
+                  <UnorderedListOutlined className="text-[18px] rotate-180" />
+                )}
+                {open ? "Закрывать" : "Категория"}
               </Button>
               <Input
-                placeholder="Kупить xочу.. "
+                placeholder="Kупить xочу.."
                 className="search_input text-[14px] font-medium w-[520px] h-[46px] py-[15px] px-[36px] rounded-md"
                 prefix={<SearchOutlined />}
-                // suffix={<CloseSquareOutlined />}
               />
             </div>
 
-            <div className={`w-[100%] flex gap-[80px] absolute bg-[#fff] left-0 z-20 py-[30px] px-[20px] ${open ? ' top-[110px] duration-300 opacity-1' : "top-[-1200px] duration-300 opacity-0"} drawer`}>
-                <div>
-                    {
-                      categories.map((e,i) => {
-                        return (
-                          <div key={i} onClick={() => getSub(e.id)} className=' hover:bg-[#FF6F14] hover:text-white mt-[10px] flex items-center justify-between w-[440px] h-[50px] py-[35px] px-[59px] bg-[#F0F0F0] rounded-xl cursor-pointer card '>
-                              <PhoneOutlined className='w-[60px] h-[60px] text-[20px]'/>
-                              <p>{e.name}</p>
-                              <ArrowRightOutlined />
-                          </div>
-                        )
-                      })
-                    }
-                </div>
-                <div className="mt-[7px] border-l-[1px] pl-[60px]">
-                    <p className="text-[27px] font-bold mb-[20px]">Tovarlar</p>
-                    {
-                      subcategories.map((e,i) => {
-                        return (
-                          <div key={i}>
-                                <p className="font-semibold text-[16px] mb-4 cursor-pointer">{e.name}</p>
-                          </div>
-                        )
-                      })
-                    }
-                </div>
+            <div className={`w-[100%] flex gap-[80px] absolute bg-[#fff] left-0 z-20 py-[30px] px-[20px] ${open ? 'drawer open' : 'drawer'}`}>
+              <div>
+                {categories.map((e, i) => (
+                  <div 
+                    key={i} 
+                    onClick={() => getSub(e.id)} 
+                    className="hover:bg-[#FF6F14] hover:text-white mt-[10px] flex items-center justify-between w-[440px] h-[50px] py-[35px] px-[59px] bg-[#F0F0F0] rounded-xl cursor-pointer card"
+                  >
+                    <PhoneOutlined className="w-[60px] h-[60px] text-[20px]" />
+                    <p>{e.name}</p>
+                    <ArrowRightOutlined />
+                  </div>
+                ))}
+              </div>
+              <div className="mt-[7px] border-l-[1px] pl-[60px]">
+                <p className="text-[27px] font-bold mb-[20px]">Tovarlar</p>
+                {subcategories.map((e, i) => (
+                  <div key={i}>
+                    <p className="font-semibold text-[16px] mb-4 cursor-pointer">{e.name}</p>
+                  </div>
+                ))}
+              </div>
             </div>
 
             <div className="flex items-center gap-[15px]">
-              <Badge count={2} >
-                <Avatar shape="square" size="large" className="bg-[#F0F0F0] cursor-pointer" >
-                    <HeartOutlined className="text-[20px] text-[black]" />
-                </Avatar>
-              </Badge>
-              <Badge count={6} >
+              <Badge count={2}>
                 <Avatar shape="square" size="large" className="bg-[#F0F0F0] cursor-pointer">
-                    <BarChartOutlined className="text-[20px] text-[black]"/>
+                  <HeartOutlined className="text-[20px] text-[black]" />
                 </Avatar>
               </Badge>
-              <Badge count={7} >
+              <Badge count={6}>
                 <Avatar shape="square" size="large" className="bg-[#F0F0F0] cursor-pointer">
-                    <ShoppingCartOutlined className="text-[20px] text-[black]" />
+                  <BarChartOutlined className="text-[20px] text-[black]" />
                 </Avatar>
               </Badge>
-              <Avatar size="large" icon={<UserOutlined className="text-[20px] text-[black]"/>} className="bg-[#F0F0F0] cursor-pointer"/>
+              <Badge count={7}>
+                <Avatar shape="square" size="large" className="bg-[#F0F0F0] cursor-pointer">
+                  <ShoppingCartOutlined className="text-[20px] text-[black]" />
+                </Avatar>
+              </Badge>
+              <Avatar size="large" icon={<UserOutlined className="text-[20px] text-[black]" />} className="bg-[#F0F0F0] cursor-pointer" />
             </div>
           </div>
         </Container>
